dc35c70b5f7ea611b803ea153d6ad3e86ed796f9
Roman Shevchuk <roman.shevchuk@rubrikkgroup.com>
2023-12-05 14:54:34 +0200

    first commit

---
+++ WizudaCodingExercise/Program.cs
@@ -0,0 +1,51 @@
+﻿// Create an instance of LRUCache with a capacity of 3
+using WizudaCodingExercise.Services;
+
+LRUCache<string, int> cache = new LRUCache<string, int>(3);
+
+// Add some items to the cache
+cache.AddOrUpdate("One", 1);
+cache.AddOrUpdate("Two", 2);
+cache.AddOrUpdate("Three", 3);
+
+// Try to retrieve items from the cache
+if (cache.TryGetValue("One", out int value1))
+{
+    Console.WriteLine("Value for key 'One': " + value1);
+}
+else
+{
+    Console.WriteLine("Key 'One' not found.");
+}
+
+if (cache.TryGetValue("Two", out int value2))
+{
+    Console.WriteLine("Value for key 'Two': " + value2);
+}
+else
+{
+    Console.WriteLine("Key 'Two' not found.");
+}
+
+// Add another item (causing eviction)
+cache.AddOrUpdate("Four", 4);
+
+// Try to retrieve the evicted item
+if (cache.TryGetValue("Three", out int value3))
+{
+    Console.WriteLine("Value for key 'Three': " + value3);
+}
+else
+{
+    Console.WriteLine("Key 'Three' not found (evicted).");
+}
+
+// Try to retrieve a non-existing key
+if (cache.TryGetValue("Five", out int value5))
+{
+    Console.WriteLine("Value for key 'Five': " + value5);
+}
+else
+{
+    Console.WriteLine("Key 'Five' not found.");
+}
---
+++ WizudaCodingExercise/Services/LRUCache.cs
@@ -0,0 +1,92 @@
+﻿
+namespace WizudaCodingExercise.Services
+{
+    public class LRUCache<TKey, TValue>
+    {
+        private readonly int capacity;
+        private readonly Dictionary<TKey, CacheNode> cache;
+        private readonly LinkedList<CacheNode> lruList;
+
+        private readonly object lockObject = new object();
+
+        public LRUCache(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentException("Capacity must be greater than zero.");
+            }
+
+            this.capacity = capacity;
+            cache = new Dictionary<TKey, CacheNode>(capacity);
+            lruList = new LinkedList<CacheNode>();
+        }
+
+        public void AddOrUpdate(TKey key, TValue value)
+        {
+            lock (lockObject)
+            {
+                if (cache.TryGetValue(key, out CacheNode node))
+                {
+                    node.Value = value;
+                    node.AccessTime = DateTime.Now;
+                    lruList.Remove(node);
+                    lruList.AddLast(node);
+                }
+                else
+                {
+                    if (cache.Count >= capacity)
+                        Evict();
+
+
+                    CacheNode newNode = new CacheNode(key, value);
+                    cache.Add(key, newNode);
+                    lruList.AddLast(newNode);
+                }
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            lock (lockObject)
+            {
+                if (cache.TryGetValue(key, out CacheNode node))
+                {
+                    // Move the accessed item to the end of the LRU list
+                    node.AccessTime = DateTime.Now;
+                    lruList.Remove(node);
+                    lruList.AddLast(node);
+
+                    value = node.Value;
+                    return true;
+                }
+
+                value = default;
+                return false;
+            }
+        }
+
+        private void Evict()
+        {
+            // Implement your eviction strategy here (LRU, LFU, etc.)
+            CacheNode lruNode = lruList.First.Value;
+            cache.Remove(lruNode.Key);
+            lruList.RemoveFirst();
+
+            Console.WriteLine($"Evicted: {lruNode.Key}");
+        }
+
+        private class CacheNode
+        {
+            public TKey Key { get; }
+            public TValue Value { get; set; }
+            public DateTime AccessTime { get; set; }
+
+            public CacheNode(TKey key, TValue value)
+            {
+                Key = key;
+                Value = value;
+                AccessTime = DateTime.Now;
+            }
+        }
+    }
+}
